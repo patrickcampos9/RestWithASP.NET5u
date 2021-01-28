7338e911aba49662ae542968b1ae9820110b8193
patrickcampos9 <patrickcampos77@hotmail.com>
2021-01-28 10:05:50 -0300

    s

---
+++ 01_RestWithASP.NET5u_ScaffoldViaVs/RestWithASP.NET5u/RestWithASP.NET5u/Model/Penson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP.NET5u.Model
+{
+    public class Penson
+    {
+
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
